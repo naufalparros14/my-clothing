20f26359a6621ed270ca71504c41197ad8a4d917
naufalparros14 <naufalparros14@gmail.com>
2024-06-25 14:49:39 +0700

    first commit

---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,27 @@
+<div style="padding-top: 100px">
+    <!-- Content Row -->
+    <div style="text-align: center" >
+
+       
+
+       
+
+        <!-- Earnings (Monthly) Card Example -->
+        <div style="text-align: center">
+        <h1 style="text-align: center">Selamat Datang  <?php echo $this->session->userdata('username') ?></h1>
+
+    </div>
+    <div style="padding-top: 25px"> <a href="<?=
+                 base_url('admin/data_barang/')?>" class="badge badge-info"><i
+                 class="fas fa-edit"></i>Data Barang</a>
+                 <a href="<?=
+                 base_url('admin/invoice/')?>" class="badge badge-danger"><i
+                 class="fas fa-edit"></i>Data Pemesanan</a>
+        </div>
+
+
+        <!-- Pending Requests Card Example -->
+
+    <!-- Content Row -->
+</div>
+</div>
